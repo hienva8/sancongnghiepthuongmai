e87b63c97a048dfac4af47fb79ea342831c93e73
danchoithuthiet <danchoithuthiet@gmail.com>
2010-10-29 16:09:33 +0000

    Mã lệnh giao diện chương trình

---
+++ DuLieuDA/Source code chương trình/Source code giao diện/Public/buy-sale-com.php
@@ -0,0 +1,336 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+<link href="CSS/style.css" rel="stylesheet" type="text/css" />
+<link href="CSS/style_menu.css" rel="stylesheet" type="text/css" />
+<link href="CSS/style_buy_sale.css" rel="stylesheet" type="text/css" />
+<script type="text/javascript" src="../Library/js/jquery_menu.js"></script>
+<script type="text/javascript" src="../Library/js/script.js"></script>
+</head>
+
+<body>
+<div id="banner">
+	<div id="ig">
+    	<a href=""><img src="Interface/UK.GIF" border="0px" alt="English" width="35" height="20" align="middle"/></a>
+        <a href=""><img src="Interface/VIE.GIF" border="0px" alt="Việt Nam" width="35" height="20" align="middle" /></a>    
+  	</div>
+    <div id="menu_banner">
+      <ul>
+       	<li><a href="" class="line">Hỏi-Đáp</a></li>
+        <li><a href="" class="line">Trợ giúp</a></li>
+        <li><a href="" class="line">Liên hệ</a></li>
+        <li ><a href=""class="line">Giới thiệu</a></li>
+      </ul>
+  </div>
+    <div id="menu-container">
+    	<ul id="navigationMenu">
+			<center>
+            <li><a href="#" class="normalMenu">Trang chủ</a></li>
+			<li><a href="#" class="normalMenu">Chào bán</a></li>
+			<li><a href="#" class="normalMenu">Chào mua</a></li>
+			<li><a href="#" class="normalMenu">Sản phẩm </a></li>
+			<li><a href="#" class="normalMenu">Doanh nghiệp</a></li>
+            <li><a href="#" class="normalMenu">Dịch vụ</a></li>
+			<li><a href="#" class="normalMenu">Đấu giá</a></li>
+            </center>
+		</ul>
+  </div>   
+</div>
+<div id="bg_2"> 
+<div id="se-in">
+  <div id="search">
+      <input type="text" value="Search products" name="info_search" size="45" height="22"/>
+    	<select name="se_country">
+          <option value="0" selected>Select country</option>
+          <option value="1" selected>Việt Nam</option>
+          <option value="2" selected>English</option>
+      </select>
+  </div>
+     <div id="button-se"><input name="search" type="image" src="Interface/search.jpg"/></div>
+</div>
+  <h1><font color="#FFFF00">Popular Searches:</font><font color="#FFFFFF"> Máy nghiền, máy xúc, xe nâng, máy kéo, ... </font></h1>
+</div>
+<div id="info">
+  <div class="right">
+    <div class="mem_login">
+    	<h1>Chào mừng đến VnTrade.com</h1>
+        <center><input type="image" src="Interface/reg_1.jpg" /></center>
+        <div id="sig"><h2>Nào hãy cùng tham gia!</h2></div><div id="sig_2"><a href="#">Sign in</a></div>
+        <div id="cls"></div>        
+    </div>
+        <div class="business">
+       	  <h1>Hỗ trợ trực tuyến</h1>
+            <div id="info_help">            
+            <p style="padding:7px 0px 5px 45px;"><a href="/XML_DTDD/ymsgr:sendIM?hailyhoihan_2803"><img src="http://opi.yahoo.com/online?u=hailyhoihan_2803&amp;m=g&amp;t=2" alt="" border="0" /></a></p>
+            <center><p class="style_help_2">Bộ phận kỹ thuật</p></center>
+            <p style="padding:7px 0px 0px 45px;"><a href="/XML_DTDD/ymsgr:sendIM?hailyhoihan_2803"><img src="http://opi.yahoo.com/online?u=hailyhoihan_2803&amp;m=g&amp;t=2" alt="" border="0" /></a></p>
+            <center><p class="style_help_2">Bộ phận kinh doanh</p></center>           
+            </div>
+          <center>
+              <img src="Interface/khung_2.jpg" height="15" align="left" width="220"/>
+          </center>
+      	</div>
+        <div class="mem_login"><div class="adds">
+        	<center>
+            	<img src="Interface/Advertisement/adds_1.jpg"/>
+            	<img src="Interface/Advertisement/add_2.jpg" />
+                <img src="Interface/Advertisement/add_3.jpg"/>
+           </center>
+        </div></div>
+  </div>
+    <div class="right-center">
+    	<div class="bor_1">
+        	<img src="Interface/home-1.jpg" width="16" height="16" />&nbsp;<span class="st_1"><a href="#">Home</a>&nbsp;<b> ></b>&nbsp;<a href="#"> Doanh Nghiệp</a></span>
+        </div>
+ 		<div class="cates">
+        	<fieldset>            
+            	<legend>Ngành nghề</legend>            	 
+                <table border="0">
+                	<tr>
+						<td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+                        <td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+                        <td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+                        <td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+					</tr>
+                    <tr>
+						<td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+                        <td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+                        <td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+                        <td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+					</tr>
+                   <tr>
+						<td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+                        <td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+                        <td><img src="Interface/icon_2.jpg" width="5" height="9" /><a href="#">Máy móc</a><span class="number">(99)</span></td>
+                        
+				  </tr>
+                  
+                </table>
+				
+            </fieldset>    
+        </div>
+        <div id="cates-info">
+        	<div class="contact">
+            
+            </div>
+        	<h1>Doanh nghiệp</h1>
+            	<div class="box">
+            	<div class="check">
+               	  <input type="checkbox" name="checkbox" id="c123" value="123"/>
+                </div>
+              	<div class="box_2">
+                	<div class="img">
+                    	<img src="Interface/Advertisement/add_2.jpg" align="left"/>                    
+                    	<h2><a href="#">TÊN CÔNG TY HAY TÊN SẢN PHẨM</a></h2>
+                        <label>Mô tả ngắn của công ty về doanh nghiệp với số kí tự chừng 255</label>
+                    </div>
+                    <div class="c-info">
+               	  	  <h2><a href="#">CÔNG TY TNHH THƯƠNG MẠI & ĐẦU TƯ SONG PHÁT</a></h2>
+                      	<div class="box_3">
+                      	  <img src="Interface/icon_3.gif" align="left"/>
+                      	  <h3>Thành viên VIP</h3>
+   				  	  </div>
+                       	<div class="box_3">	<img src="Interface/vietnam-icon.png" align="left"/>
+                       						  <h3>VIỆT NAM</h3>
+       				  	</div>
+                        <div class="but"><input type="image" src="Interface/contact.jpg"/></div>
+                    </div>                    
+              </div>
+              
+            </div>
+            	<div class="box">
+            	<div class="check">
+               	  <input type="checkbox" name="checkbox" id="c123" value="123"/>
+                </div>
+              <div class="box_2">
+                	<div class="img">
+                    	<img src="Interface/Advertisement/adds_1.jpg" align="left"/>                    
+                    	<h2><a href="#">TÊN CÔNG TY HAY TÊN SẢN PHẨM</a></h2>
+                        <label>Mô tả ngắn của công ty về doanh nghiệp với số kí tự chừng 255</label>
+                    </div>
+                    <div class="c-info">
+               	  	  <h2><a href="#">CÔNG TY TNHH THƯƠNG MẠI & ĐẦU TƯ SONG PHÁT</a></h2>
+                      	<div class="box_3">	<img src="Interface/icon_3.gif" align="left"/>
+                       						  <h3>Thành viên VIP</h3>
+       				  	</div>
+                       	<div class="box_3">	<img src="Interface/vietnam-icon.png" align="left"/>
+                       						  <h3>VIỆT NAM</h3>
+       				  	</div>
+                        <div class="but"><input type="image" src="Interface/contact.jpg"/></div>
+                    </div>                    
+              </div>
+              
+            </div>
+            	<div class="box">
+            	<div class="check">
+               	  <input type="checkbox" name="checkbox" id="c123" value="123"/>
+                </div>
+              <div class="box_2">
+                	<div class="img">
+                    	<img src="Interface/Advertisement/add_2.jpg" align="left"/>                    
+                    	<h2><a href="#">TÊN CÔNG TY HAY TÊN SẢN PHẨM</a></h2>
+                        <label>Mô tả ngắn của công ty về doanh nghiệp với số kí tự chừng 255</label>
+                    </div>
+                    <div class="c-info">
+               	  	  <h2><a href="#">CÔNG TY TNHH THƯƠNG MẠI & ĐẦU TƯ SONG PHÁT</a></h2>
+                      	<div class="box_3">	<img src="Interface/icon_3.gif" align="left"/>
+                       						  <h3> Thành viên VIP</h3>
+       				  	</div>
+                       	<div class="box_3">	<img src="Interface/vietnam-icon.png" align="left"/>
+                       						  <h3>VIỆT NAM</h3>
+       				  	</div>
+                        <div class="but"><input type="image" src="Interface/contact.jpg"/></div>
+                    </div>                    
+              </div>
+              
+            </div>
+            	<div class="box">
+            	<div class="check">
+               	  <input type="checkbox" name="checkbox" id="c123" value="123"/>
+                </div>
+              <div class="box_2">
+                	<div class="img">
+                    	<img src="Interface/Advertisement/adds_1.jpg" align="left"/>                    
+                    	<h2><a href="#">TÊN CÔNG TY HAY TÊN SẢN PHẨM</a></h2>
+                        <label>Mô tả ngắn của công ty về doanh nghiệp với số kí tự chừng 255</label>
+                    </div>
+                    <div class="c-info">
+               	  	  <h2><a href="#">CÔNG TY TNHH THƯƠNG MẠI & ĐẦU TƯ SONG PHÁT</a></h2>
+                      	<div class="box_3">	<img src="Interface/icon_3.gif" align="left"/>
+                       						  <h3>Thành viên VIP</h3>
+       				  	</div>
+                       	<div class="box_3">	<img src="Interface/vietnam-icon.png" align="left"/>
+                       						  <h3>VIỆT NAM</h3>
+       				  	</div>
+                        <div class="but"><input type="image" src="Interface/contact.jpg"/></div>
+                    </div>                    
+              </div>
+              
+            </div>
+          		 <div class="box">
+            	<div class="check">
+               	  <input type="checkbox" name="checkbox" id="c123" value="123"/>
+                </div>
+              <div class="box_2">
+                	<div class="img">
+                    	<img src="Interface/Advertisement/add_2.jpg" align="left"/>                    
+                    	<h2><a href="#">TÊN CÔNG TY HAY TÊN SẢN PHẨM</a></h2>
+                        <label>Mô tả ngắn của công ty về doanh nghiệp với số kí tự chừng 255</label>
+                    </div>
+                    <div class="c-info">
+               	  	  <h2><a href="#">CÔNG TY TNHH THƯƠNG MẠI & ĐẦU TƯ SONG PHÁT</a></h2>
+                      	<div class="box_3">	<img src="Interface/icon_3.gif" align="left"/>
+                       						  <h3>Thành viên VIP</h3>
+       				  	</div>
+                       	<div class="box_3">	<img src="Interface/vietnam-icon.png" align="left"/>
+                       						  <h3>VIỆT NAM</h3>
+       				  	</div>
+                        <div class="but"><input type="image" src="Interface/contact.jpg"/></div>
+                    </div>                    
+              </div>
+              
+            </div>
+            	<div class="box">
+            	<div class="check">
+               	  <input type="checkbox" name="checkbox" id="c123" value="123"/>
+                </div>
+              <div class="box_2">
+                	<div class="img">
+                    	<img src="Interface/Advertisement/adds_1.jpg" align="left"/>                    
+                    	<h2><a href="#">TÊN CÔNG TY HAY TÊN SẢN PHẨM</a></h2>
+                        <label>Mô tả ngắn của công ty về doanh nghiệp với số kí tự chừng 255</label>
+                    </div>
+                    <div class="c-info">
+               	  	  <h2><a href="#">CÔNG TY TNHH THƯƠNG MẠI & ĐẦU TƯ SONG PHÁT</a></h2>
+                      	<div class="box_3">	<img src="Interface/icon_3_1.gif" align="left"/>
+                       						  <h3>Thành viên VIP</h3>
+       				  	</div>
+                       	<div class="box_3">	<img src="Interface/vietnam-icon.png" align="left"/>
+                       						  <h3>VIỆT NAM</h3>
+       				  	</div>
+                        <div class="but"><input type="image" src="Interface/contact.jpg"/></div>
+                    </div>                    
+              </div>
+              
+            </div>
+            	<div class="box">
+            	<div class="check">
+               	  <input type="checkbox" name="checkbox" id="c123" value="123"/>
+                </div>
+              <div class="box_2">
+                	<div class="img">
+                    	<img src="Interface/Advertisement/add_2.jpg" align="left"/>                    
+                    	<h2><a href="#">TÊN CÔNG TY HAY TÊN SẢN PHẨM</a></h2>
+                        <label>Mô tả ngắn của công ty về doanh nghiệp với số kí tự chừng 255</label>
+                    </div>
+                    <div class="c-info">
+               	  	  <h2><a href="#">CÔNG TY TNHH THƯƠNG MẠI & ĐẦU TƯ SONG PHÁT</a></h2>
+                      	<div class="box_3">	<img src="Interface/icon_3_1.gif" align="left"/>
+                       						  <h3>Thành viên VIP</h3>
+       				  	</div>
+                       	<div class="box_3">	<img src="Interface/vietnam-icon.png" align="left"/>
+                       						  <h3>VIỆT NAM</h3>
+       				  	</div>
+                        <div class="but"><input type="image" src="Interface/contact.jpg"/></div>
+                    </div>                    
+              </div>
+              
+            </div>
+         	   <div id="contact_2">
+            	<div id="contact_3">
+                	<img src="Interface/icon_6.jpg" align="left" />
+                    <h2>Chọn</h2>
+                </div>
+                <div id="contact_but">
+                	<input type="image" src="Interface/contact.jpg" />
+                </div>
+                <div id="show-info">
+                	<h2>Hiển thị theo dạng: </h2>                	               
+                    <img src="Interface/icon_5.jpg" align="left" />
+                    <img src="Interface/icon_4.jpg" align="left" />
+                </div>
+            </div>
+            <div id="num_page">
+            	<div id="page_1">
+                	<h2>Trang 1/196 </h2>                    
+                </div>
+                <div id="page_2">
+                	<h2>Đầu << 1 | 2 | 3 | 4 | 6 | 7 | 8 >> Cuối </h2>
+                </div>
+                <div id="count_page">
+                	<h2>Tổng số mẫu tin: 196</h2>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div id="menu_bot">
+    	<ul>
+        	<li class="line"><a href="">Trang chủ</a></li>
+        	<li class="line"><a href="">Giới thiệu</a></li>
+        	<li class="line"><a href="">Sản phẩm</a></li>
+        	<li class="line"><a href="">Chào mua</a></li>        	
+            <li class="line"><a href="">Chào bán</a></li>
+            <li class="line"><a href="">Doanh nghiệp</a></li>
+            <li class="line"><a href="">Dịch vụ</a></li>
+            <li class="line"><a href="">Đấu giá</a></li>
+            <li class="line"><a href="">Hỏi-Đáp</a></li>
+            <li class="line"><a href="">Trợ giúp</a></li>
+        	<li><a href="">Liên hệ</a></li>
+  </ul>                                                            
+</div>
+<div id="private">
+	<h1>© 2005 muaban.net.<br />
+  		Giấy phép hoạt động số: 196/GP-BC do Bộ VHTT cấp 20/10/2005.<br />
+  		www.muaban.net có trách nhiệm chuyển tải thông tin. Không chịu bất kỳ trách nhiệm nào từ các tin này. Xem chi tiết
+    </h1>
+</div>
+<div id="coppyright">
+	<h1>Copyright Notice © 2010 by Vn-Trade.com</h1>
+</div>
+
+</body>
+</html>
